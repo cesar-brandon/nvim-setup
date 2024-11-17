c2c38aaee84a82f22f67147a066e143c938efdb5
Cesar Brandon <cesarbrandon000@gmail.com>
2024-11-17 17:04:37 -0500

    ✨ Add PHP and Go language support

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,4 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {
-  --   transparent_mode = false,
-  -- } },
   {
     "folke/tokyonight.nvim",
     opts = {
@@ -12,54 +9,4 @@ return {
       },
     },
   },
-  -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  -- },
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = true,
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --     integrations = {
-  --       aerial = true,
-  --       alpha = true,
-  --       cmp = true,
-  --       dashboard = true,
-  --       flash = true,
-  --       gitsigns = true,
-  --       headlines = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       leap = true,
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       markdown = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       navic = { enabled = true, custom_bg = "lualine" },
-  --       neotest = true,
-  --       neotree = true,
-  --       noice = true,
-  --       notify = true,
-  --       semantic_tokens = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       treesitter_context = true,
-  --       which_key = true,
-  --     },
-  --   },
-  -- },
 }
